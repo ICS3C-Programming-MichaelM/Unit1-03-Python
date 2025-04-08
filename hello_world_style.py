5a1dd08f5fbaa49d8e9d4230a760d641a30ef5bd
Micthelle michael <micthelle.chinwendu.michael@stu.ocsb.ca>
2025-04-08 16:38:46 +0000

    added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Micthelle Michael
+# Date: april 08, 2025
+# This program describes what the program does in style
+
+
+def main():
+    print("Hello, world! with proper style!")
+    print("I am testing my function here.")
+
+
+if __name__ == "__main__":
+    main()
